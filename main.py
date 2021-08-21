116c581688321570df7b1db690ee034aa9167d1c
atosti <5588048+atosti@users.noreply.github.com>
2021-08-21 14:53:05 -0700

    Audio slices are now roughly 10s and avoid slicing mid-word

--- main.py
+++ main.py
@@ -4,36 +4,58 @@ import os
 one_sec = 1000 #miliseconds
 
 
-# Basic concept
-# 1. Loop through the entire song and slice it into 10 second chunks.
-#   - dice() can do this, but how to check for middle of words?
-# 2. If in in the middle of a word, try to move forward 1ms until dead air is found.
-# 3. Trim all dead air from the front of any audio clips.
-# 4. Attach 0.1s of dead air to the ends.
-# 5. Save all the chunks into a folder.
 
 folder = "voices/jc-denton/"
 file_name = "deus-ex-denton-lines.wav"
 sound = AudioSegment.from_file(folder+file_name, format="wav")
-duration = len(sound) / one_sec # Duration in seconds
-# Slice into 10-second chunks
+duration = len(sound) # Duration in miliseconds
 results = []
-slices = int(duration/10) + int(duration / int(duration))
-for i in range(0, slices-1):
-    results.append(sound[i*one_sec:(i+10)*one_sec])
-# Add a final slice to handle leftover audio that's less than 10s
-if duration > int(duration):
-    results.append(sound[(slices-1)*10*one_sec:])
+
+# Start at 0ms, until duration in ms
+# Create a left and right pointer, starting 10 seconds apart
+# Check if a slice can be made without being in the center of a word.
+#   1. Look at last ms of potential slice, if <= -50 DBFS, it's silence.
+#   2. Otherwise, move right pointer to the left one ms at a time.
+#      Do this until silence is found.
+# Create the audio slice, and add it to the results list
+# Set left to the right pointer and set right to be +10s above that.
+left = 0
+right = 10*one_sec
+while(right < duration):
+    last_ms = sound[right-1:right]
+    loudness = last_ms.dBFS
+    # TODO - Remove infinite loop possibility for > 10s of silence
+    while(loudness > -60):
+        right -= 1
+        last_ms = sound[right-1:right]
+        loudness = last_ms.dBFS
+    results.append(sound[left:right])
+    left = right
+    right = left + 10*one_sec
+
 # Export the slices into their own directory as wav files
 if not os.path.isdir(folder+"slices/"):
     sub_folder = os.mkdir(folder+"slices/")
+sound = sound.set_frame_rate(22050).set_channels(1) # Mono w/ 22050 sample rate
 cnt = 1
 for item in results:
-    item.export(folder+"slices/slice"+str(cnt)+".wav", format="wav")
+    item.export(
+        folder+"slices/slice"+str(cnt)+".wav",
+         format="wav",
+    )
     cnt += 1
 
-# FIXME - Remove this later, it's just for quick testing
-# results[0].export(folder+"slices/slice"+str(cnt)+".wav", format="wav")
+# 04:10 in the part 2 vid
+# Audio slices:
+# Audio needs to be mono. Cut into lengths < 12 seconds. Don't split words.
+# Sample rate of 22050. Save cuts in a numerical ordering.
+# Text:
+# Proofread and edit the text to be perfect.
+# Accurate punctuation is important to determine pauses and mood.
+# Create string transcripts for the wav slices.
+# 1. Trim spaces before/after the text.
+# 2. Finally, add end of line characters after the text ('~').
+# 3. Split the spreadsheet into sets of 50 lines for validation sets.
 
 # TODO - Loudness code, implement it as part of slicing
 # # Less than or equal to -50?
