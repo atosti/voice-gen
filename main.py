dd8dbb02ef274f8d449f74f0b2fa983483da2a67
atosti <5588048+atosti@users.noreply.github.com>
2021-08-24 23:37:08 -0700

    Rough transcription now implemented. Needs adjustment for accuracy.

--- main.py
+++ main.py
@@ -1,54 +1,97 @@
 from pydub import AudioSegment
+from multiprocessing import Pool
 import os
+import speech_recognition as sr
+import functools
 
-one_sec = 1000 #miliseconds
+one_sec = 1000 # In miliseconds
 
-
-
-folder = "voices/jc-denton/"
-file_name = "deus-ex-denton-lines.wav"
-sound = AudioSegment.from_file(folder+file_name, format="wav")
-sound = sound.set_frame_rate(22050).set_channels(1) # Mono w/ 22050 sample rate
-duration = len(sound) # Duration in miliseconds
-results = []
-
-# Start at 0ms, until duration in ms
-# Create a left and right pointer, starting 10 seconds apart
-# Check if a slice can be made without being in the center of a word.
-#   1. Look at last ms of potential slice, if <= -50 DBFS, it's silence.
-#   2. Otherwise, move right pointer to the left one ms at a time.
-#      Do this until silence is found.
-# Create the audio slice, and add it to the results list
-# Set left to the right pointer and set right to be +10s above that.
-left = 0
-right = 10*one_sec
-while(right < duration):
-    last_ms = sound[right-1:right]
-    loudness = last_ms.dBFS
-    # TODO - Remove infinite loop possibility for > 10s of silence
-    while(loudness > -60):
-        right -= 1
-        last_ms = sound[right-1:right]
+def slice_audio(base_dir, file_name):
+    audio = AudioSegment.from_file(base_dir + file_name, format = "wav")
+    audio = audio.set_frame_rate(22050).set_channels(1) # Mono w/ 22050 sample rate
+    silence = AudioSegment.silent(duration = 0.1 * one_sec)  # Duration in milliseconds
+    # Export slices into their own directory as wav files. Approx 10s long w/ 0.1s of silence at end. Avoids cutting mid-word
+    duration = len(audio) # Duration in miliseconds
+    sounds = []
+    left = 0
+    right = 10*one_sec
+    while(right < duration):
+        last_ms = audio[right-1:right]
         loudness = last_ms.dBFS
-    results.append(sound[left:right])
-    left = right
-    right = left + 10*one_sec
+        # TODO - Remove infinite loop possibility for > 10s of silence
+        while(loudness > -60):
+            right -= 1
+            last_ms = audio[right-1:right]
+            loudness = last_ms.dBFS
+        sounds.append(audio[left:right] + silence)
+        left = right
+        right = left + 10*one_sec
+    cnt = 1
+    os.mkdir(f'{base_dir}slices/')
+    for sound in sounds:
+        sound.export(
+            f'{base_dir}slices/slice{cnt:04}.wav',
+            format = "wav",
+        )
+        cnt += 1
+    return
 
-# Export the slices into their own directory as wav files
-if not os.path.isdir(folder+"slices/"):
-    sub_folder = os.mkdir(folder+"slices/")
-cnt = 1
-for item in results:
-    item.export(
-        folder+"slices/slice"+str(cnt)+".wav",
-         format="wav",
-    )
-    cnt += 1
+def slice_transcribe(file, base_dir):
+    r = sr.Recognizer()
+    transcript = ""
+    with sr.AudioFile(base_dir + "slices/" + file) as source:
+        audio = r.record(source)  # Read the audio file
+        try:
+            transcript = r.recognize_sphinx(audio) + "\n"
+            print(str(file) + " complete.") # TODO - Remove later.
+        except sr.UnknownValueError:
+            transcript = "\n"
+            print("Sphinx could not understand audio in: " + str(file))
+        except sr.RequestError as e:
+            transcript = "\n"
+            print("Sphinx error; {0}".format(e))
+    return transcript
+
+def transcribe(base_dir):
+    transcriptions = []
+    files = os.listdir(base_dir + "slices/")
+    with Pool(os.cpu_count() - 1) as p:
+        transcriptions = p.map(functools.partial(slice_transcribe, base_dir=base_dir), files)
+    with open(base_dir + "transcript.txt", "w") as textfile:
+        textfile.writelines(transcriptions)
+    return
+
+def main():
+    base_dir = "voices/jc-denton/"
+    file_name = "deus-ex-denton-lines.wav"
+    # Only slice the audio if it hasn't been sliced before
+    if not os.path.isdir(base_dir + "slices/"):
+        slice_audio(base_dir, file_name)
+    # Only transcribe the audio if it hasn't been done before
+    if not os.path.isfile(base_dir + "transcript.txt"):
+        transcribe(base_dir)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+# FIXME - Remove later
+# https://pypi.org/project/pocketsphinx/
+# https://cmusphinx.github.io/wiki/tutorialtuning/
+# https://cmusphinx.github.io/wiki/pocketsphinx_pronunciation_evaluation/
+# https://cmusphinx.github.io/wiki/tutorialsphinx4/
+# https://cmusphinx.github.io/wiki/tutoriallm/
+
+# TODO: Add 0.1 seconds of silence to the end of the file 
 
 # 04:10 in the part 2 vid
-# Audio slices:
-# Audio needs to be mono. Cut into lengths < 12 seconds. Don't split words.
-# Sample rate of 22050. Save cuts in a numerical ordering.
 # Text:
 # Proofread and edit the text to be perfect.
 # Accurate punctuation is important to determine pauses and mood.
@@ -56,12 +99,3 @@ for item in results:
 # 1. Trim spaces before/after the text.
 # 2. Finally, add end of line characters after the text ('~').
 # 3. Split the spreadsheet into sets of 50 lines for validation sets.
-
-# TODO - Loudness code, implement it as part of slicing
-# # Less than or equal to -50?
-# loudness = curr.dBFS
-# # Need to determine whether it's quiet. Can I compare against the avg of the whole thing?
-# print(loudness)
-
-
-
